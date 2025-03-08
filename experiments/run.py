7d4a1691eb60752210e509764a7c6e5831b07eac
Matt <mgws3@cam.ac.uk>
2025-03-08 17:40:02 +0000

    Added an automated experiment for running different K values.

--- experiments/run.py
+++ experiments/run.py
@@ -6,8 +6,12 @@ import os
 
 
 
-def top_k_experiment(n_runs=3, algorithm="macura_top_k", env_config="macura_inverted_pendulum"): 
-    pass
+def top_k_experiment(n_k,n_runs=3, algorithm="macura_top_k", env_config="macura_inverted_pendulum"): 
+    
+    for k in range(2, n_k):
+        for i in range(n_runs): 
+            os.system(f"python -m mbrl.examples.main algorithm={algorithm} overrides={env_config} algorith.k={k} seed={i}")
+            print(f"Run {i+1} completed.")
 
 
 
